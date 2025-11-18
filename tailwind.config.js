db8a3a981f5a6de7a6d01d47cd84d6207dcc8a85
Jan-Paul van den Berg <janpaulvdberg@gmail.com>
2025-11-18 13:17:08 +0200

    Fixed tailwind stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  // OLD: content: ["./**/*.{html,js}"], <-- This was scanning EVERYTHING
+  
+  // NEW: This only scans HTML and JS files in your main folder
+  content: ["./*.{html,js}"], 
+  
   theme: {
     extend: {},
   },
